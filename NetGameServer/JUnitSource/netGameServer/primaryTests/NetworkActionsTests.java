2781a3f081ba8b3534cc94203eab9b27550c55ac
Krazick <markjsmith60@gmail.com>
2021-09-15 16:11:44 -0400

    Expand NetworkActionsTests to test remove and getActionXML

--- NetGameServer/JUnitSource/netGameServer/primaryTests/NetworkActionsTests.java
+++ NetGameServer/JUnitSource/netGameServer/primaryTests/NetworkActionsTests.java
@@ -13,15 +13,34 @@ import netGameServer.primary.NetworkActions;
 @DisplayName ("Network Actions Tests")
 class NetworkActionsTests {
 	NetworkActions networkActions;
+	NetworkActions networkActionsBeta;
 	NetworkAction networkAction1;
 	NetworkAction networkAction2;
+	NetworkAction networkAction1B;
+	NetworkAction networkAction2B;
+	NetworkAction networkAction3B;
+	NetworkAction networkAction4B;
+	NetworkAction networkAction5B;
 	
 	@BeforeEach
 	void setUp () throws Exception {
 		networkActions = new NetworkActions ();
 		networkAction1 = new NetworkAction (101, "Complete");
-		networkAction1.setActionXML ("<Action actor=\"GameServer\" class=\"ge18xx.round.action.Action\" number=\"100\">");
+		networkAction1.setActionXML ("<Action actor=\"GameServer\" class=\"ge18xx.round.action.Action\" number=\"101\">");
 		networkAction2 = new NetworkAction (102, "Pending");
+
+		networkActionsBeta = new NetworkActions ();
+		networkAction1B = new NetworkAction (101, "Complete");
+		networkAction1B.setActionXML ("<Action actor=\"GameServer\" class=\"ge18xx.round.action.Action\" number=\"101\">");
+		networkAction2B = new NetworkAction (102, "Complete");
+		networkAction2B.setActionXML ("<Action actor=\"GameServer\" class=\"ge18xx.round.action.Action\" number=\"102\">");
+		networkAction3B = new NetworkAction (103, "Complete");
+		networkAction3B.setActionXML ("<Action actor=\"GameServer\" class=\"ge18xx.round.action.Action\" number=\"103\">");
+		networkAction4B = new NetworkAction (104, "Complete");
+		networkAction4B.setActionXML ("<Action actor=\"GameServer\" class=\"ge18xx.round.action.Action\" number=\"104\">");
+		networkAction5B = new NetworkAction (105, "Complete");
+		networkAction5B.setActionXML ("<Action actor=\"GameServer\" class=\"ge18xx.round.action.Action\" number=\"105\">");
+
 	}
 
 	@Test
@@ -104,10 +123,68 @@ class NetworkActionsTests {
 			assertEquals ("<ActionNotRecieved/>", tActionXML);
 			
 			tActionXML = networkActions.getActionXMLAt (0);
-			assertEquals ("<Action actor=\"GameServer\" class=\"ge18xx.round.action.Action\" number=\"100\">", tActionXML);
+			assertEquals ("<Action actor=\"GameServer\" class=\"ge18xx.round.action.Action\" number=\"101\">", tActionXML);
 			
 			tStatus = networkActions.getStatusAt (0);
 			assertEquals ("Complete", tStatus);
+		}
+	}
+	
+	@Nested
+	@DisplayName ("Test Removal of Network Actions")
+	class RemovalNetworkActionTests {
+		@Test
+		@DisplayName ("Based on an Action Number")
+		void getRemovalWithActionNumberTests () {
+			int tActionNumber;
+			
+			networkActionsBeta.addNetworkAction (networkAction1B);
+			networkActionsBeta.addNetworkAction (networkAction2B);
+			networkActionsBeta.addNetworkAction (networkAction3B);
+			networkActionsBeta.addNetworkAction (networkAction4B);
+			networkActionsBeta.addNetworkAction (networkAction5B);
+			assertEquals (5, networkActionsBeta.getCount ());
+			assertEquals (105, networkActionsBeta.getLastNetworkActionNumber ());
+			
+			tActionNumber = 105;
+			networkActionsBeta.remove (tActionNumber);
+			assertEquals (4, networkActionsBeta.getCount ());
+			assertEquals (104, networkActionsBeta.getLastNetworkActionNumber ());
+			
+			tActionNumber = 104;
+			networkActionsBeta.remove (tActionNumber);
+			assertEquals (3, networkActionsBeta.getCount ());
+			assertEquals (103, networkActionsBeta.getLastNetworkActionNumber ());
+			
+			networkActionsBeta.addNetworkAction (networkAction4B);
+			networkActionsBeta.addNetworkAction (networkAction5B);
+			assertEquals (5, networkActionsBeta.getCount ());
+			tActionNumber = 106;
+			networkActionsBeta.remove (tActionNumber);
+			assertEquals (5, networkActionsBeta.getCount ());
+
+		}
+
+	}
+	@Nested
+	@DisplayName ("Test Get ActionXML of Network Actions")
+	class GetActionXMLNetworkActionTests {
+		@Test
+		@DisplayName ("Based on an Action Number")
+		void getGetActionXMLWithActionNumberTests () {
+			int tActionNumber;
+			
+			networkActionsBeta.addNetworkAction (networkAction1B);
+			networkActionsBeta.addNetworkAction (networkAction2B);
+			networkActionsBeta.addNetworkAction (networkAction3B);
+			networkActionsBeta.addNetworkAction (networkAction4B);
+			networkActionsBeta.addNetworkAction (networkAction5B);
+			assertEquals (5, networkActionsBeta.getCount ());
+			tActionNumber = 106;
+			assertEquals ("<ActionNotRecieved/>", networkActionsBeta.getActionXMLFor (tActionNumber));
+
+			tActionNumber = 105;
+			assertEquals ("<Action actor=\"GameServer\" class=\"ge18xx.round.action.Action\" number=\"105\">", networkActionsBeta.getActionXMLFor (tActionNumber));
 		}
 	}
 }
